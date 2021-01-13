2988a4d3d99ea4c5e022deacfcd6e08e4257a611
R0MaNbI4 <wb15935725@mail.ru>
2021-01-14 05:31:13 +0800

    Lesson5 (#5)
    Co-authored-by: R0MaNbI4 <petrov_roman98@mail.ru>

---
+++ Level1/Lesson5/src/Worker.java
@@ -0,0 +1,74 @@
+public class Worker {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
+
+    Worker (String fullName, String position, String email, String phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void getInfo() {
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("E-mail: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+}
