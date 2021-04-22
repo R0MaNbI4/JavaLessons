3385920bbc89c92e19808e9b356172f751243805
R0MaNbI4 <wb15935725@mail.ru>
2021-04-22 19:25:02 +0800

    Lesson3.3 (#19)
    * Lesson3.3 initial commit
    
    * Lesson3.3 save message history
    
    * Lesson3.3 load message history
    
    * Lesson3.3 auto-scrolling
    
    Co-authored-by: R0MaNbI4 <petrov_roman98@mail.ru>

---
+++ Level3/Lesson3/src/ru/geekbrains/chat/client/gui/ClientChatFrame.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.chat.client.gui;
+
+import ru.geekbrains.chat.client.data.MessageLog;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.function.Consumer;
+
+public class ClientChatFrame extends JFrame implements ChatFrameInteraction {
+    private final ChatFrame chatFrame;
+
+    public ClientChatFrame(Consumer<String> messageConsumer) {
+        this.chatFrame = new ChatFrame("Chat", messageConsumer);
+    }
+
+    @Override
+    public void append(String message) {
+        chatFrame.getChatArea().append(message);
+        chatFrame.getChatArea().append("\n");
+        chatFrame.scrollDown();
+    }
+}
